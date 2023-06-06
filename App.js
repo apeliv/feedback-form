54181cdcf33b5ee22b7a4172428697cd23175d51
Anđelo Pelivan <andelo.pelivan95@hotmail.com>
2023-06-06 17:33:02 +0200

    Create input components and implement initial UI design and layout

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
+import DataInput from "./components/DataInput";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.textInputSection}>
+        <DataInput label="Name" placeholder="Enter name" />
+        <DataInput label="Number" placeholder="Enter number" />
+        <DataInput label="Email" placeholder="Enter email" />
+      </View>
+      <View>
+        <Text style={styles.text}>Share your experience in scaling</Text>
+      </View>
+      <Button title="Send" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#fff",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+  },
+  textInputSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    backgroundColor: "#fff",
+  },
+  text: {
+    margin: 2,
+    fontSize: 20,
+    color: "#3944bc",
+    fontWeight: "bold",
   },
 });
---
+++ components/DataInput.js
@@ -0,0 +1,30 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+const DataInput = ({ label, placeholder }) => {
+  return (
+    <View style={styles.textInputContainer}>
+      <Text style={styles.text}>{label}</Text>
+      <TextInput style={styles.textInput} placeholder={placeholder} />
+    </View>
+  );
+};
+
+export default DataInput;
+
+const styles = StyleSheet.create({
+  textInputContainer: {
+    width: "40%",
+    margin: 10,
+  },
+  textInput: {
+    borderWidth: 1,
+    padding: 16,
+    borderRadius: 10,
+    fontSize: 18,
+  },
+  text: {
+    margin: 2,
+    fontSize: 20,
+    color: "#3944bc",
+    fontWeight: "bold",
+  },
+});
